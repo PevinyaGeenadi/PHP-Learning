8d8d0bd9ae5789837975b1b3711499bdfe0311de
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-09-24 10:04:32 +0530

    update

--- Page links/index.php
+++ Page links/index.php
@@ -11,11 +11,12 @@
 
 <?php
 // Defining parameters
-$product_id = 10;
+$product_id = 10;   //variable
 $user = "JohnDoe";
 
 // Creating the URL dynamically with query parameters
-$link = "details.php?product_id=" . $product_id . "&user=" . urlencode($user);
+$link = "details.php? product_id=" . $product_id . "&user=" . urlencode($user);
+
 ?>
 
 <!-- Link to another page with dynamic parameters -->
