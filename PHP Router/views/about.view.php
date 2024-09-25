56f4a6b5f1916895d8c3bd69ad59cab8059264f3
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-09-25 14:52:31 +0530

    add php sessions & authentication

---
+++ PHP Router/views/about.view.php
@@ -0,0 +1,23 @@
+<?php
+require ('partials/header.php');
+?>
+
+
+    <?php
+      require ('partials/nav.php');
+      ?>
+
+    <?php require ('partials/banner.php'); ?>
+
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <p><b>Welcome to our About Page! </b> We aim to share helpful insights and information on various topics.
+                Our mission is to provide clear and engaging content that informs and inspires our readers.
+                Thank you for visiting, and we hope you enjoy exploring our site!</p>
+
+        </div>
+    </main>
+
+<?php
+require ('partials/footer.php');
+?>
