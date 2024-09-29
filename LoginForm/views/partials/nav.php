05b3879af809dbf32d4d2cdcd70895408f22d304
PevinyaGeenadi <haputhanthri200@gmail.com>
2024-09-29 18:19:01 +0530

    update

--- LoginForm/views/partials/nav.php
+++ LoginForm/views/partials/nav.php
@@ -3,16 +3,16 @@
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
                 <div class="flex-shrink-0">
-                    <img class="h-10 w-20" src="partials/mlogo.png" alt="Your Company">
+                    <img class="h-10 w-20" src="views/mlogo.png" alt="Your Company">
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/controllers/index.php" class="<?= $_SERVER['REQUEST_URI'] == '/'? 'bg-gray-900 text-white': 'text-gray-300'?>  hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+                        <a href="/controllers/index.php" class="<?= $_SERVER['REQUEST_URI'] == '/index.php'? 'bg-gray-900 text-white': 'text-gray-300'?> hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Home</a>
                         <a href="/controllers/about.php" class="<?= $_SERVER['REQUEST_URI'] == '/about.php'? 'bg-gray-900 text-white': 'text-gray-300'?> hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">About</a>
                         <a href="/contact.php" class="<?= $_SERVER['REQUEST_URI'] == '/contact.php'? 'bg-gray-900 text-white': 'text-gray-300'?> hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Contacts</a>
-                        <a href="/login.php" class="<?= $_SERVER['REQUEST_URI'] == '/contact.php'? 'bg-gray-900 text-white': 'text-gray-300'?> hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium">Login</a>
-                    </div>
+                        <a href="/controllers/loginForm.php" class="<?= $_SERVER['REQUEST_URI'] == '/'? 'bg-gray-900 text-white': 'text-gray-300'?>  hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Login</a>
+                       </div>
                 </div>
             </div>
         </div>
